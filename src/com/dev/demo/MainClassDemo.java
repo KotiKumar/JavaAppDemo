74cd3f5227b66b8fe8b28ba5ec34d56afd8e6ae3
Kotikumar <kotikumaramanikanta@gmail.com>
2020-09-28 17:29:34 +0530

    second commit

--- src/com/dev/demo/MainClassDemo.java
+++ src/com/dev/demo/MainClassDemo.java
@@ -4,6 +4,10 @@ public class MainClassDemo {
 
 	public static void main(String[] args) {
 		System.out.println("MainClass");
+		System.out.println("MainClass1");
+		System.out.println("MainClass2");
+		System.out.println("MainClass3");
+		System.out.println("MainClass4");
 
 	}
 
